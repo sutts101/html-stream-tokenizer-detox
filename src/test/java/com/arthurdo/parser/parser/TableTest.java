230d6af319ea493af73358d23ca360c51b0a82e5
Ian Sutton <sutts101@gmail.com>
2011-06-20 10:45:17 +0800

    Coverage using cobertura

---
+++ src/test/java/com/arthurdo/parser/parser/TableTest.java
@@ -0,0 +1,12 @@
+package com.arthurdo.parser.parser;
+
+import com.arthurdo.parser.Table;
+import org.junit.Test;
+
+public class TableTest {
+
+    @Test
+    public void dummy() {
+        new Table();
+    }
+}
